7dbf335c0c041cdb1d823a0ed5cd69e663c9267b
PaigeQQ <127891142+PaigeQQ@users.noreply.github.com>
2026-08-24 04:05:45 +0800

    更新 1Re.js

--- PoJieJB/1Re.js
+++ PoJieJB/1Re.js
@@ -67,7 +67,8 @@ const bundle = {
 const listua = {
   'Drops': { name: 'premium', id: 'forever_unlimited_time_discounted_80_int', cm: 'sjb' },  //Drops外语
   'Readle': { name: 'Premium', id: 'com.hello.german.yearly', cm: 'sja' },  //Readle-德语学习 及同作者另外几款语言教育APP	
-  
+  'Language%20Learning': { name: 'premium', id: 'language_sub_lifetime', cm: 'sjb' },  //Wordy (苹果商店ID号:id6670703228)
+
   // 'PicPure': { name: 'PicPure Pro', id: 'LifetimeAccess2', cm: 'sjc' },  //PicPure-照片清理大师
   // 'VHSCamera': { name: 'pro', id: 'vhscam_pro_lifetime', cm: 'sjc' },  //VHS相机
   // 'Image2PDF': { name: 'Convertly Pro', id: 'life_time_id', cm: 'sjc' },  //Convertly-图片转PDF/相机扫描
